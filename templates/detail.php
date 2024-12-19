bab06486ffc78632609852fe8cda1c7ef676e5ef
Mzomba <117861946+Mzomba@users.noreply.github.com>
2024-12-19 16:25:17 +0200

    upload

---
+++ templates/detail.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap-5.0.2-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <script src="../bootstrap-5.0.2-dist/js/boostrap.min.js" defer></script>
+    <title>detail</title>
+</head>
+<body>
+<!--<header id="header"></header> -->
+<?php include "nav.php" ?>
+
+<?php
+#<!-- BACK END -->
+require 'connection.php';
+
+$id=$_GET['id'];
+$sql = "SELECT name,image,description,created_at, text FROM information WHERE informationID = $id";
+$result = $conn->query($sql);
+
+        while($info = $result->fetch_assoc()) {
+            ?>     
+
+<div class="article_pic">
+    <img class="img" src="data:image;charset=utf8;base64,<?php echo base64_encode($info['image']); ?>" alt="">
+    <div class="grad p-2">
+    <h3 class="article_title text-light mt-2"><?= $info['name']?></h3>
+    <p class="text-light text-wrap" style="width:30rem;"><?= $info['description']?></p>
+    </div>
+</div>
+<div class="content-container container">
+    <p class="introduction mt-2"><?= $info['text']?>
+</p>
+</div>
+<?php
+}
+?>
+<?php include "footer.php" ?>
+</body>
+</html>
---
+++ templates/intro_section.php
@@ -0,0 +1,12 @@
+    <div class="intro_container">
+        <div class="container">
+            <h1 class="text-center pt-2" style="color:#fff;">Welcome to EswatiniPedia</h1>
+            <div class="d-flex align-item-center justify-content-center">
+                <p class="fs-5 d-flex align-item-center justify-content-center" style="color:#fff; width:38rem;">
+                        This is an online encyclopedia where you can explore different categories like ceremonies, trees, animals and many more.
+                        The aim of this site is to help people interested on knowlendge  
+                        and other activities happening in the country.
+                </p>
+            </div>
+        </div>
+    </div>
